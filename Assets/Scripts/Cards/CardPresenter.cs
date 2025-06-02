be5520c4513305d60b8b90bb0181f5fc06d2cd85
Juan Haro <jcharomo@gmail.com>
2025-06-02 10:34:56 -0600

    Add base card

---
+++ Assets/Scripts/Cards/CardPresenter.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardPresenter : MonoBehaviour
+{
+    public List<CardData> cards;
+    public CardView view;
+
+    public Sprite grabAttackSprite;
+    public Sprite grabDefenseSprite;
+    public Sprite punchAttackSprite;
+    public Sprite punchDefenseSprite;
+    public Sprite kickAttackSprite;
+    public Sprite kickDefenseSprite;
+
+    public void SetCard(CardData _data, CardView _view)
+    {
+        _view.powerDisplay.text = _data.power.ToString();
+        _view.cardNameDisplay.text = _data.cardName;
+        _view.attackSlotssDisplay.text = _data.attackSlots.ToString();
+        _view.sprite.sprite = _data.sprite;
+        _view.type.sprite = GetTypeSprite(_data.type);
+    }
+
+    Sprite GetTypeSprite(CardType type)
+    {
+        switch (type)
+        {
+            case CardType.GrabAttack:
+                return grabAttackSprite;
+            case CardType.GrabDefense:
+                return grabDefenseSprite;
+            case CardType.PunchAttack:
+                return punchAttackSprite;
+            case CardType.PunchDefense:
+                return punchDefenseSprite;
+            case CardType.KickAttack:
+                return kickAttackSprite;
+            case CardType.KickDefense:
+            default:
+                return kickDefenseSprite;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            int i = Random.Range(0, cards.Count);
+            SetCard(cards[i], view);
+        }
+    }
+
+    private void Start()
+    {
+        SetCard(cards[0], view);
+
+    }
+}
---
+++ Assets/Scripts/Cards/CardView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    public TextMeshPro powerDisplay;
+    public TextMeshPro cardNameDisplay;
+    public TextMeshPro attackSlotssDisplay;
+    public SpriteRenderer sprite;
+    public SpriteRenderer type;
+}
